ee8897fa092144c08a2159e3232fd43932c833c8
stephalexandra <44372966+stephalexandra@users.noreply.github.com>
2019-02-23 01:20:20 -0500

    Add files via upload

---
+++ u4l3/p2.py
@@ -0,0 +1,6 @@
+import time
+for x in range(1000,0,-1):
+	time.sleep(.05)
+	print(x)
+
+print("Blast Off!")
---
+++ u4l3/p4.py
@@ -0,0 +1,5 @@
+for t in range(0,51): 
+	print("t = " + str(t))
+	a = 10000 * (2.718) ** (.09 * t)
+	print("a = " + str(a))
+	print()
